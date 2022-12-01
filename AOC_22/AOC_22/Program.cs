437d90e45edb9a19e47020fd444aea06d13c0327
Preston Glenn <pglenn@hagerty.com>
2022-12-01 02:32:47 -0500

    day 1

---
+++ AOC_22/AOC_22/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_1;
+
+bool topthree = true;
+var res = new solution(topthree);
+var final = topthree ? res._topThreeResult : res._result;
+Console.WriteLine(final);
